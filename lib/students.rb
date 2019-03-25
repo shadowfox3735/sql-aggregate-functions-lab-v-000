e90724b1860d2c6ce51f174b41d974f359a78df6
shadowfox3735 <normanpan180@gmail.com>
2019-03-25 23:20:54 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -9,12 +9,13 @@ def lowest_student_gpa
 end
 
 def average_student_gpa
-  "AVG(gpa)"
+  "AVG(gpa) FROM students;"
 end
 
 def total_tardies_for_all_students
-  "SUM(tardies)"
+  "SUM(tardies) FROM students;"
 end
 
 def average_gpa_for_9th_grade
+  "AVG(gpa) "
 end
